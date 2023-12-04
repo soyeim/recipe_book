d4ef4b76c02299389cec82502ad9ae0ca7d86d65
soyeim <146144729+soyeim@users.noreply.github.com>
2023-12-04 05:05:26 +0000

    generate photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_04_021613) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_050423) do
   create_table "dishes", force: :cascade do |t|
     t.string "image"
     t.string "name"
     t.string "description"
     t.integer "recipes_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
